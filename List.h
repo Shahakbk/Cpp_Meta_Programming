ff056c217bce59f59142314f771dc9c1c7c409e0
Shahakbk <shahakbk@gmail.com>
2020-01-18 18:49:27 +0200

    first draft of List.h done

--- List.h
+++ List.h
@@ -5,17 +5,17 @@
 #ifndef OOP_HW5_LIST_H
 #define OOP_HW5_LIST_H
 
-/// declaring List
+/// Declaring List.
 template<typename ...TT>
 struct List {};
 
-/// empty List should contain only size 0.
+/// Assert that an empty List contains only size=0.
 template<>
 struct List<> {
     constexpr static int size = 0;
 };
 
-/// List of 1 element, contains the element, next is empty List, and size is 1.
+/// In a singleton list, the list contains the single element, next is an empty List, and size is 1.
 template<typename T>
 struct List<T> {
     typedef T head;
@@ -23,12 +23,72 @@ struct List<T> {
     constexpr static int size = 1;
 };
 
-/// List, contains head, next List, and Size.
+/// Non singleton list contains head, next List, and size.
 template<typename T, typename ...TT>
 struct List<T, TT...> {
     typedef T head;
     typedef List<TT...> next;
     constexpr static int size = (sizeof ...(TT) + 1);
+};
+
+/// Declaring PrependList.
+template<typename ...TT>
+struct PrependList {};
+
+/// Puts the element in the head of List, and the rest in Next of List.
+template<typename T, typename ...TT>
+struct PrependList<T, List<TT...> > {
+   typedef List<T, TT...> list;
+   constexpr static int  size = (sizeof ...(TT)+1);
+};
+
+/// Declaring GetAtIndex.
+template<int N, typename T>
+struct GetAtIndex {};
+
+/// GetAtIndex(0) for a List with a single type.
+template<typename T>
+struct GetAtIndex<0, List<T> > {
+    typedef T value;
+};
+
+/// GetAtIndex(0) for a List with several types.
+template<typename T, typename ...TT>
+struct GetAtIndex<0, List<T, TT...> >{
+    typedef T value;
+};
+
+/// GetAtIndex for a List, only if N>=0, continues recursively until N=0, value will be the type.
+template<int N, typename T, typename ...TT>
+struct GetAtIndex<N, List<T, TT...> > {
+    static_assert(N < 0, "INDEX IS ILLEGAL (smaller than 0)");
+    typedef typename GetAtIndex<N - 1, List<TT...> >::value value;
+};
+
+/// Declaring SetAtIndex.
+template<int N,typename U, typename T>
+struct SetAtIndex {};
+
+/// SetAtIndex(0) for List with a single type.
+template <typename U, typename T>
+struct SetAtIndex<0, U, List<T> > {
+    typedef List<U> list;
+};
+
+/// SetAtIndex(0) for List with several types.
+template <typename U, typename T, typename...TT>
+struct SetAtIndex<0, U, List<T, TT...> > {
+    typedef List<U, TT...> list;
+};
+
+/// SetAtIndex for other indexes.
+/// If we are here then N!=0, so make a recursive definition till index is 0.
+/// Every iteration we take the first type, and join it to the rest where we try to put the original type at index N-1.
+/// The value will be set in the end of the recursion when N=0.
+template <int N, typename U, typename T, typename ...TT>
+struct SetAtIndex<N, U, List<T, TT...> > {
+    static_assert(N < 0, "Index  is smaller than 0");
+    typedef typename PrependList<T, SetAtIndex <N - 1, U, List<TT...> > >::list list;
 };
 
 #endif //OOP_HW5_LIST_H
--- Subject.h
+++ Subject.h
@@ -31,7 +31,7 @@ template<typename T> class Subject {
 
   void removeObserver(Observer<T>& observer_to_remove) {
    for (typename vector<Observer<T>*>::const_iterator it = observers_vec.cbegin(); it != observers_vec.cend(); ++it) {
-     if (*it == &observer_to_remove){
+     if (*it == &observer_to_remove) {
        observers_vec.erase(it);
        return;
      }
