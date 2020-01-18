14e82b9c33845bf7faf06711949726ae427e2404
Shahakbk <shahakbk@gmail.com>
2020-01-19 00:18:42 +0200

    rearrangement of directories

---
+++ part1/Observer.h
@@ -0,0 +1,12 @@
+#ifndef OOP_HW5_OBSERVER_H
+#define OOP_HW5_OBSERVER_H
+
+template<typename T>
+class Observer {
+
+ public:
+
+  virtual void handleEvent(const T& t) = 0;
+};
+
+#endif //OOP_HW5_OBSERVER_H
