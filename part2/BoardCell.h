18e23ec224b329a4cd7a2a1521e367c461381cdd
Shahakbk <shahakbk@gmail.com>
2020-01-19 04:17:31 +0200

    fixed static assertions in List.h & added in BoardCell.h

--- part2/BoardCell.h
+++ part2/BoardCell.h
@@ -7,10 +7,12 @@
 
 #include "CellType.h"
 #include "Direction.h"
+#include <iostream>
 
 /// The c'tor receives the actual values representing the cell, therefore using constexpr to set the values the struct fields will hold.
 template<CellType T, Direction D, int L>
 struct BoardCell {
+    static_assert(L >= 0, "BoardCell: LENGTH IS ILLEGAL (must be non-negative)");
     constexpr static CellType type = T;
     constexpr static Direction direction = D;
     constexpr static int length = L;
